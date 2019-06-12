a33d119d473c9d8dceaaba9510d325a2535160c9
Fernando Araujo <developgadget@gmail.com>
2019-06-11 19:30:27 -0500

    spring init

---
+++ DemoMongoDB/src/test/java/comspring/mongodb/DemoMongoDB/DemoMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package comspring.mongodb.DemoMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
